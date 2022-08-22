8707300278089e520587ebf24f01bdb09df738d1
nerdydev2904 <nerdcoredevelopment@gmail.com>
2022-08-22 19:35:27 +0530

    Removed a redundant TODO

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/MainActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/MainActivity.java
@@ -30,10 +30,6 @@ import com.nerdcoredevelopment.game2048champsfinal.manager.MainManager;
 import java.util.List;
 
 // TODO -> ## User Experience ## : Adjust the layout files by making tweaks for Tablet users
-/* TODO -> If score and best score displays are apart make sure that they are visible, problem is
-           arising when a user just reaches best score and merge animation starts and midway the
-           user presses Undo
-*/
 /* TODO -> Use the 'Switch' selection control for toggling the settings when we create the settings
            page. We can refer the Selection Controls section project from the Material Design Course
            hands-on projects
