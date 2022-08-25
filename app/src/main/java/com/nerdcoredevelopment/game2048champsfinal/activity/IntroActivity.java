78ba2059faa8063dabfa2f763a23e7b560ed89f8
nerdydev2904 <nerdcoredevelopment@gmail.com>
2022-08-25 11:28:22 +0530

    Changing the name of splash activity to intro activity

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/IntroActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/IntroActivity.java
@@ -2,22 +2,20 @@ package com.nerdcoredevelopment.game2048champsfinal.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.appcompat.widget.AppCompatTextView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.airbnb.lottie.LottieAnimationView;
 import com.nerdcoredevelopment.game2048champsfinal.R;
 
-@SuppressLint("CustomSplashScreen")
-public class CustomSplashActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
+
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +25,24 @@ public class CustomSplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_custom_splash);
+        setContentView(R.layout.activity_intro);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        
-        new Handler().postDelayed(new Runnable() {
+
+        new CountDownTimer(4000, 4000) {
             @Override
-            public void run() {
-                Intent intent = new Intent(CustomSplashActivity.this, MainActivity.class);
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }.start();
     }
 }
