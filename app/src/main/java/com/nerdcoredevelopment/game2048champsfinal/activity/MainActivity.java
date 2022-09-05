fa50e1da5762cd1bf9181ea6f2b26c94260cabd8
nerdydev2904 <nerdcoredevelopment@gmail.com>
2022-09-05 23:35:27 +0530

    Completed the setup for visiting the pages of Leaderboards and Shop in the upcoming code changes in the NavigationFragment

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/MainActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -109,14 +110,14 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onNavigationFragmentSettingsClicked() {
-        SettingsFragment fragment = new SettingsFragment();
+    public void onNavigationFragmentPreGameClicked() {
+        PreGameFragment fragment = new PreGameFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right,
                 R.anim.enter_from_right, R.anim.exit_to_right);
         transaction.addToBackStack(null);
-        transaction.replace(R.id.full_screen_fragment_container, fragment, "SETTINGS_FRAGMENT")
+        transaction.replace(R.id.full_screen_fragment_container, fragment, "PREGAME_FRAGMENT")
                 .commit();
     }
 
@@ -133,15 +134,25 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onNavigationFragmentPreGameClicked() {
-        PreGameFragment fragment = new PreGameFragment();
+    public void onNavigationFragmentLeaderboardsClicked() {
+        Toast.makeText(MainActivity.this, "Leaderboards Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNavigationFragmentSettingsClicked() {
+        SettingsFragment fragment = new SettingsFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right,
                 R.anim.enter_from_right, R.anim.exit_to_right);
         transaction.addToBackStack(null);
-        transaction.replace(R.id.full_screen_fragment_container, fragment, "PREGAME_FRAGMENT")
+        transaction.replace(R.id.full_screen_fragment_container, fragment, "SETTINGS_FRAGMENT")
                 .commit();
+    }
+
+    @Override
+    public void onNavigationFragmentShopClicked() {
+        Toast.makeText(MainActivity.this, "Shop Clicked", Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/fragment/NavigationFragment.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/fragment/NavigationFragment.java
@@ -30,7 +30,9 @@ public class NavigationFragment extends Fragment {
 
         FrameLayout pregameLinearLayout = view.findViewById(R.id.fragment_navigation_pregame_frame_layout);
         FrameLayout announcementsLinearLayout = view.findViewById(R.id.fragment_navigation_announcements_linear_layout);
+        FrameLayout leaderboardsLinearLayout = view.findViewById(R.id.fragment_navigation_leaderboards_linear_layout);
         FrameLayout settingsLinearLayout = view.findViewById(R.id.fragment_navigation_settings_linear_layout);
+        FrameLayout shopLinearLayout = view.findViewById(R.id.fragment_navigation_shop_linear_layout);
 
         pregameLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +50,14 @@ public class NavigationFragment extends Fragment {
                 }
             }
         });
+        leaderboardsLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.onNavigationFragmentLeaderboardsClicked();
+                }
+            }
+        });
         settingsLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,16 +66,24 @@ public class NavigationFragment extends Fragment {
                 }
             }
         });
+        shopLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.onNavigationFragmentShopClicked();
+                }
+            }
+        });
 
         return view;
     }
 
     public interface OnNavigationFragmentInteractionListener {
-        void onNavigationFragmentSettingsClicked();
-
-        void onNavigationFragmentAnnouncementsClicked();
-
         void onNavigationFragmentPreGameClicked();
+        void onNavigationFragmentAnnouncementsClicked();
+        void onNavigationFragmentLeaderboardsClicked();
+        void onNavigationFragmentSettingsClicked();
+        void onNavigationFragmentShopClicked();
     }
 
     @Override
