5f60f3c876f25accae7b96c4b3f00952f6b51eaf
nerdydev2904 <nerdcoredevelopment@gmail.com>
2022-09-06 13:59:44 +0530

    Added a layer for lottie animations for individual cells and a grid-sized lottie animation view for bigger animations

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/GameLayoutProvider.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/GameLayoutProvider.java
@@ -13,6 +13,7 @@ import androidx.appcompat.widget.AppCompatTextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.nerdcoredevelopment.game2048champsfinal.enums.GameModes;
 
 public class GameLayoutProvider {
@@ -27,6 +28,7 @@ public class GameLayoutProvider {
         constraintSet.setDimensionRatio(gameFrameLayout.getId(), gameMode.getGameLayoutProperties().getDimensionRatio());
         constraintSet.applyTo(rootGameConstraintLayout);
 
+        // Adding a layer of background cells
         GridLayout gameBackgroundGridLayout = new GridLayout(context);
         gameBackgroundGridLayout.setId(R.id.game_background_grid_layout);
         gameBackgroundGridLayout.setRowCount(gameMode.getRows());
@@ -55,6 +57,7 @@ public class GameLayoutProvider {
         }
         gameBackgroundGridLayout.setPadding(padding, padding, padding, padding);
 
+        // Adding a layer of actual game cells
         GridLayout gameGridLayout = new GridLayout(context);
         gameGridLayout.setId(R.id.game_grid_layout);
         gameGridLayout.setRowCount(gameMode.getRows());
@@ -80,7 +83,37 @@ public class GameLayoutProvider {
         }
         gameGridLayout.setPadding(padding, padding, padding, padding);
 
+        // Adding a layer of lottie animation views for each individual game cell
+        GridLayout gameCellLottieLayout = new GridLayout(context);
+        gameCellLottieLayout.setId(R.id.game_cell_lottie_layout);
+        gameCellLottieLayout.setRowCount(gameMode.getRows());
+        gameCellLottieLayout.setColumnCount(gameMode.getColumns());
+        for (int i = 0; i < gameMode.getRows(); i++) {
+            for (int j = 0; j < gameMode.getColumns(); j++) {
+                LottieAnimationView lottieView = new LottieAnimationView(context);
+                lottieView.setTag("gameCellLottie" + i + j);
+                lottieView.setVisibility(View.INVISIBLE);
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                params.height = 1;
+                params.width = 1;
+                params.topMargin = params.bottomMargin = params.leftMargin = params.rightMargin = padding;
+                params.rowSpec = GridLayout.spec(i,1f);
+                params.columnSpec = GridLayout.spec(j,1f);
+                params.setGravity(Gravity.FILL);
+                lottieView.setLayoutParams(params);
+                gameCellLottieLayout.addView(lottieView);
+            }
+        }
+        gameCellLottieLayout.setPadding(padding, padding, padding, padding);
+
+        // Adding a layer which consists of single full grid sized lottie animation view
+        LottieAnimationView gridLottieView = new LottieAnimationView(context);
+        gridLottieView.setId(R.id.grid_lottie_view);
+        gridLottieView.setVisibility(View.GONE);
+
         gameFrameLayout.addView(gameBackgroundGridLayout);
         gameFrameLayout.addView(gameGridLayout);
+        gameFrameLayout.addView(gameCellLottieLayout);
+        gameFrameLayout.addView(gridLottieView);
     }
 }
