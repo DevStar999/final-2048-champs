90bdab606cbbea1b34aeb9315117c4a5607388a6
nerdydev2904 <nerdcoredevelopment@gmail.com>
2022-09-06 12:59:48 +0530

    Completed the implementation of a few buttons in the SettingsFragment

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/fragment/SettingsFragment.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/fragment/SettingsFragment.java
@@ -1,10 +1,13 @@
 package com.nerdcoredevelopment.game2048champsfinal.fragment;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +18,12 @@ import com.nerdcoredevelopment.game2048champsfinal.R;
 
 public class SettingsFragment extends Fragment {
     private OnSettingsFragmentInteractionListener mListener;
+    private AppCompatImageView backButton;
+    private LinearLayout facebookLinearLayout;
+    private LinearLayout instagramLinearLayout;
+    private LinearLayout twitterLinearLayout;
+    private LinearLayout privacyLinearLayout;
+    private LinearLayout termsLinearLayout;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -25,13 +34,7 @@ public class SettingsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_settings, container, false);
-
-        // Back button in the title text of the fragment
-        AppCompatImageView backButton = view.findViewById(R.id.title_back_settings_fragment_button);
+    private void settingOnClickListeners() {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,6 +43,64 @@ public class SettingsFragment extends Fragment {
                 }
             }
         });
+        // TODO -> Check if we get redirected to the fb page inside FB app as well
+        facebookLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.facebook.com/Nerdcore-Development-109351035183956"));
+                startActivity(browserIntent);
+            }
+        });
+        // TODO -> Check if we get redirected to the ig handle inside IG app as well
+        instagramLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.instagram.com/nerdcoredev/"));
+                startActivity(browserIntent);
+            }
+        });
+        // TODO -> Check if we get redirected to the twitter handle inside Twitter app as well
+        twitterLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://twitter.com/NerdcoreDev"));
+                startActivity(browserIntent);
+            }
+        });
+        privacyLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://nerdcoredevelopment.com/privacy-policy.html"));
+                startActivity(browserIntent);
+            }
+        });
+        termsLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://nerdcoredevelopment.com/terms-of-service.html"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        backButton = view.findViewById(R.id.title_back_settings_fragment_button);
+        facebookLinearLayout = view.findViewById(R.id.facebook_linear_layout);
+        instagramLinearLayout = view.findViewById(R.id.instagram_linear_layout);
+        twitterLinearLayout = view.findViewById(R.id.twitter_linear_layout);
+        privacyLinearLayout = view.findViewById(R.id.privacy_policy_linear_layout);
+        termsLinearLayout = view.findViewById(R.id.terms_of_service_linear_layout);
+
+        settingOnClickListeners();
 
         return view;
     }
