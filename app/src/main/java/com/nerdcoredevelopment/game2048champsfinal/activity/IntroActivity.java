8424eedb1a6b654a553b78495d393e37292a00bd
nerdydev2904 <nerdcoredevelopment@gmail.com>
2022-09-12 08:20:05 +0530

    Completed the integration on AdMob into the project

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/IntroActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/IntroActivity.java
@@ -13,8 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.AppCompatImageView;
 
+import com.google.android.gms.ads.MobileAds;
 import com.nerdcoredevelopment.game2048champsfinal.R;
 
+/* TODO -> !! Caution !! - Always make sure to use a testing id for debugging and
+           testing purposes. Also ensure to use the Actual id just before creating a
+           release AAB or APK as then we want to have an actual Admob App Id
+*/
 public class IntroActivity extends AppCompatActivity {
 
     @SuppressLint("SourceLockedOrientationActivity")
@@ -32,6 +37,8 @@ public class IntroActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        MobileAds.initialize(this);
 
         AppCompatImageView developerLogoImageView = findViewById(R.id.developer_logo_image_view);
         developerLogoImageView.animate().alpha(1).setDuration(2000).start();
