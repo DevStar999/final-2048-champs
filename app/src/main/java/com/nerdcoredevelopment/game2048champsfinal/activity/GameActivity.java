07a8640a7c8eb0b99dd3834042144dc41dcedfe1
nerdydev2904 <nerdcoredevelopment@gmail.com>
2022-09-25 14:21:04 +0530

    Locked screen orientation

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/GameActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/GameActivity.java
@@ -307,12 +307,14 @@ public class GameActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onResume() {
         super.onResume();
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
