65f4eb99b42e7c29910b6dcf1e015ac1b59d3ef1
nerdydev2904 <nerdcoredevelopment@gmail.com>
2023-01-28 23:45:48 +0530

    Made a few adjustments to the MainActivity class as per the new code changes

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/MainActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/activity/MainActivity.java
@@ -1,33 +1,50 @@
 package com.nerdcoredevelopment.game2048champsfinal.activity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.nerdcoredevelopment.game2048champsfinal.fragments.AnnouncementsFragment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.InstallStateUpdatedListener;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.InstallStatus;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.nerdcoredevelopment.game2048champsfinal.BuildConfig;
+import com.nerdcoredevelopment.game2048champsfinal.R;
+import com.nerdcoredevelopment.game2048champsfinal.dialogs.ArrivingFeatureDialog;
+import com.nerdcoredevelopment.game2048champsfinal.dialogs.ErrorOccurredDialog;
+import com.nerdcoredevelopment.game2048champsfinal.dialogs.GameExitDialog;
+import com.nerdcoredevelopment.game2048champsfinal.dialogs.UpdateAppStaticAvailableDialog;
+import com.nerdcoredevelopment.game2048champsfinal.dialogs.UpdateAppStaticUnavailableDialog;
 import com.nerdcoredevelopment.game2048champsfinal.fragments.BlockDesignFragment;
 import com.nerdcoredevelopment.game2048champsfinal.fragments.LogoLottieFragment;
 import com.nerdcoredevelopment.game2048champsfinal.fragments.NavigationFragment;
-import com.nerdcoredevelopment.game2048champsfinal.R;
-import com.nerdcoredevelopment.game2048champsfinal.fragments.SettingsFragment;
-import com.nerdcoredevelopment.game2048champsfinal.dialogs.ArrivingFeatureDialog;
-import com.nerdcoredevelopment.game2048champsfinal.dialogs.GameExitDialog;
 import com.nerdcoredevelopment.game2048champsfinal.fragments.PreGameFragment;
+import com.nerdcoredevelopment.game2048champsfinal.fragments.SettingsFragment;
 import com.nerdcoredevelopment.game2048champsfinal.fragments.ShopFragment;
-import com.nerdcoredevelopment.game2048champsfinal.fragments.ThemesFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -131,32 +148,20 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements
         NavigationFragment.OnNavigationFragmentInteractionListener,
         PreGameFragment.OnPreGameFragmentInteractionListener,
-        AnnouncementsFragment.OnAnnouncementsFragmentInteractionListener,
         SettingsFragment.OnSettingsFragmentInteractionListener,
         BlockDesignFragment.OnBlockDesignFragmentInteractionListener,
-        ShopFragment.OnShopFragmentInteractionListener,
-        ThemesFragment.OnThemesFragmentInteractionListener {
+        ShopFragment.OnShopFragmentInteractionListener {
     private SharedPreferences sharedPreferences;
+
+    // Attributes required for In app updates feature
+    public static final int UPDATE_REQUEST_CODE = 100;
+    private AppUpdateManager appUpdateManager;
+    private InstallStateUpdatedListener installStateUpdatedListener;
 
     private void initialise() {
         sharedPreferences = getSharedPreferences("com.nerdcoredevelopment.game2048champsfinal", Context.MODE_PRIVATE);
     }
 
-    private void updateCoins(int currentCoins) {
-        sharedPreferences.edit().putInt("currentCoins", currentCoins).apply();
-        List<Fragment> fragments = new ArrayList<>(getSupportFragmentManager().getFragments());
-        for (int index = 0; index < fragments.size(); index++) {
-            Fragment currentFragment = fragments.get(index);
-            if (currentFragment != null && currentFragment.getTag() != null
-                    && !currentFragment.getTag().isEmpty()) {
-                if (currentFragment.getTag().equals("SHOP_FRAGMENT")) {
-                    ((ShopFragment) currentFragment).updateCoinsShopFragment(currentCoins);
-                }
-            }
-        }
-    }
-
-    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -167,18 +172,9 @@ public class MainActivity extends AppCompatActivity implements
         the line -> setContentView(R.layout.activity_main);
         */
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
-
-        // To Disable screen rotation and keep the device in Portrait mode
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        // To set the app theme to 'LIGHT' (even if 'DARK' theme is selected, however if user in their
-        // settings enables 'DARK' theme for our individual app, then it will override the following line
-        // no matter what)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         // To hide the navigation bar as default i.e. it will hide by itself if left unused or unattended
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -192,12 +188,13 @@ public class MainActivity extends AppCompatActivity implements
         LogoLottieFragment logoLottieFragment = new LogoLottieFragment();
         NavigationFragment navigationFragment = new NavigationFragment();
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.logo_lottie_fragment_container, logoLottieFragment, "LOGO_LOTTIE_FRAGMENT")
-                .replace(R.id.navigation_fragment_container, navigationFragment, "NAVIGATION_FRAGMENT")
+                .replace(R.id.logo_lottie_main_activity_fragment_container, logoLottieFragment, "LOGO_LOTTIE_FRAGMENT")
+                .replace(R.id.navigation_main_activity_fragment_container, navigationFragment, "NAVIGATION_FRAGMENT")
                 .commit();
+
+        setupInAppUpdate();
     }
 
-    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onResume() {
         super.onResume();
@@ -207,8 +204,6 @@ public class MainActivity extends AppCompatActivity implements
         */
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -225,6 +220,133 @@ public class MainActivity extends AppCompatActivity implements
             // Back button was pressed from fragment
             getSupportFragmentManager().popBackStack();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (appUpdateManager != null) {
+            appUpdateManager.unregisterListener(installStateUpdatedListener);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == UPDATE_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                // If the update is cancelled or fails, we can ignore this if we are implementing a 'Flexible Update'
+            }
+        }
+    }
+
+    private boolean isInternetConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) { return false; }
+        /* NetworkInfo is deprecated in API 29 so we have to check separately for higher API Levels */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            Network network = cm.getActiveNetwork();
+            if (network == null) { return false; }
+            NetworkCapabilities networkCapabilities = cm.getNetworkCapabilities(network);
+            if (networkCapabilities == null) { return false; }
+            boolean isInternetSuspended = !networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED);
+            return (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                    && !isInternetSuspended);
+        } else {
+            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+            return (networkInfo != null && networkInfo.isConnected());
+        }
+    }
+
+    private void setupInAppUpdate() {
+        installStateUpdatedListener = installState -> {
+            if (installState.installStatus() == InstallStatus.DOWNLOADED) {
+                popupSnackbarForCompleteUpdate();
+            }
+        };
+        appUpdateManager = AppUpdateManagerFactory.create(this);
+        appUpdateManager.registerListener(installStateUpdatedListener);
+    }
+
+    private void popupSnackbarForCompleteUpdate() { // Displays the snackbar notification and call to action.
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.root_layout_main_activity),
+                "An update has been downloaded", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("INSTALL", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appUpdateManager.completeUpdate();
+            }
+        });
+        snackbar.setActionTextColor(getColor(R.color.white));
+        snackbar.show();
+    }
+
+    private void launchInAppUpdateFlowForStaticButton() {
+        if (!isInternetConnected()) {
+            /* Using a feel right as of now. Since, the user may repeatedly press this button it isn't wise to show a dialog
+               for prompting the user to check their internet connection
+             */
+            Toast.makeText(MainActivity.this, "Network connection failed. Please check " +
+                    "internet connectivity", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        appUpdateManager.getAppUpdateInfo().addOnSuccessListener(new OnSuccessListener<>() {
+            @Override
+            public void onSuccess(AppUpdateInfo appUpdateInfo) {
+                int oldVersion = BuildConfig.VERSION_CODE;
+                int newVersion = appUpdateInfo.availableVersionCode();
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                    if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+                        UpdateAppStaticAvailableDialog updateAppStaticAvailableDialog =
+                                new UpdateAppStaticAvailableDialog(MainActivity.this);
+                        updateAppStaticAvailableDialog.setUpdateAppStaticAvailableDialogListener(response -> {
+                            if (response) {
+                                try {
+                                    appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.FLEXIBLE,
+                                            MainActivity.this, UPDATE_REQUEST_CODE);
+                                } catch (IntentSender.SendIntentException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                        updateAppStaticAvailableDialog.show();
+                    }
+                } else if (oldVersion == newVersion) {
+                    // Note: Even in the case when there was no internet we get UpdateAvailability.UPDATE_NOT_AVAILABLE.
+                    // So we made that check earlier
+                    new UpdateAppStaticUnavailableDialog(MainActivity.this).show();
+                } else {
+                    // This is the final error code branch
+                    new ErrorOccurredDialog(MainActivity.this, "Oops! Something went wrong").show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                new ErrorOccurredDialog(MainActivity.this, "Oops! Something went wrong").show();
+            }
+        });
+    }
+
+    private void updateCoins(int currentCoins) {
+        sharedPreferences.edit().putInt("currentCoins", currentCoins).apply();
+        List<Fragment> fragments = new ArrayList<>(getSupportFragmentManager().getFragments());
+        for (int index = 0; index < fragments.size(); index++) {
+            Fragment currentFragment = fragments.get(index);
+            if (currentFragment != null && currentFragment.getTag() != null
+                    && !currentFragment.getTag().isEmpty()) {
+                if (currentFragment.getTag().equals("SHOP_FRAGMENT")) {
+                    ((ShopFragment) currentFragment).updateCoinsShopFragment(currentCoins);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onNavigationFragmentGpgsSignInClicked() {
+        Toast.makeText(MainActivity.this, "GPGS Sign In Clicked", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -250,25 +372,8 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onNavigationFragmentAnnouncementsClicked() {
-        // If AnnouncementsFragment was opened and is currently on top, then return
-        int countOfFragments = getSupportFragmentManager().getFragments().size();
-        if (countOfFragments > 0) {
-            Fragment topMostFragment = getSupportFragmentManager().getFragments().get(countOfFragments-1);
-            if (topMostFragment != null && topMostFragment.getTag() != null && !topMostFragment.getTag().isEmpty()
-                    && topMostFragment.getTag().equals("ANNOUNCEMENTS_FRAGMENT")) {
-                return;
-            }
-        }
-
-        AnnouncementsFragment fragment = new AnnouncementsFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right,
-                R.anim.enter_from_right, R.anim.exit_to_right);
-        transaction.addToBackStack(null);
-        transaction.replace(R.id.main_activity_full_screen_fragment_container,
-                fragment, "ANNOUNCEMENTS_FRAGMENT").commit();
+    public void onNavigationFragmentAchievementsClicked() {
+        Toast.makeText(MainActivity.this, "Achievements Clicked", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -341,11 +446,6 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onAnnouncementsFragmentInteractionBackClicked() {
-        onBackPressed();
-    }
-
-    @Override
     public void onSettingsFragmentInteractionBackClicked() {
         onBackPressed();
     }
@@ -370,28 +470,6 @@ public class MainActivity extends AppCompatActivity implements
         transaction.addToBackStack(null);
         transaction.add(R.id.main_activity_full_screen_fragment_container,
                 fragment, "SHOP_FRAGMENT").commit();
-    }
-
-    @Override
-    public void onSettingsFragmentInteractionChangeThemeClicked() {
-        // If ThemesFragment was opened and is currently on top, then return
-        int countOfFragments = getSupportFragmentManager().getFragments().size();
-        if (countOfFragments > 0) {
-            Fragment topMostFragment = getSupportFragmentManager().getFragments().get(countOfFragments-1);
-            if (topMostFragment != null && topMostFragment.getTag() != null && !topMostFragment.getTag().isEmpty()
-                    && topMostFragment.getTag().equals("THEMES_FRAGMENT")) {
-                return;
-            }
-        }
-
-        ThemesFragment fragment = new ThemesFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right,
-                R.anim.enter_from_right, R.anim.exit_to_right);
-        transaction.addToBackStack(null);
-        transaction.add(R.id.main_activity_full_screen_fragment_container,
-                fragment, "THEMES_FRAGMENT").commit();
     }
 
     @Override
@@ -432,14 +510,17 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onSettingsFragmentInteractionHowToPlayClicked() {
-        // TODO -> Remove toast and implement the 'How To Play' fragment
         Toast.makeText(MainActivity.this, "'How To Play' button clicked", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onSettingsFragmentInteractionHelpClicked() {
-        // TODO -> Remove toast and implement the 'Help' fragment where we would answer FAQs
         Toast.makeText(MainActivity.this, "'Help' button clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSettingsFragmentInteractionCheckUpdatesClicked() {
+        launchInAppUpdateFlowForStaticButton();
     }
 
     @Override
@@ -467,17 +548,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onShopFragmentInteractionRestorePurchaseClicked() {
-        Toast.makeText(MainActivity.this, "Restore Purchases Clicked", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
     public void onShopFragmentInteractionUpdateCoins(int currentCoins) {
         updateCoins(currentCoins);
-    }
-
-    @Override
-    public void onThemesFragmentInteractionBackClicked() {
-        onBackPressed();
     }
 }
