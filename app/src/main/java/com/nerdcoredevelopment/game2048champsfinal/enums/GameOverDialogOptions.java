5a49c93e63c9e2399ad5f9c8670fad3c09bca70c
nerdydev2904 <nerdcoredevelopment@gmail.com>
2023-01-28 23:38:21 +0530

    Made a few adjustments to the enums of CellValues, GameLayoutProperties, GameModes and GameOverDialogOptions as per the new code changes

--- app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/enums/GameOverDialogOptions.java
+++ app/src/main/java/com/nerdcoredevelopment/game2048champsfinal/enums/GameOverDialogOptions.java
@@ -4,12 +4,23 @@ import lombok.Getter;
 
 @Getter
 public enum GameOverDialogOptions {
-    /* If the user wants to return to the main menu */
+    /* If the user wants to shop coins from the ShopFragment (First Page) */
+    SHOP_COINS,
+    /* If the user chose to use the Standard Tool Undo (First Page) */
+    STANDARD_TOOL_UNDO,
+    /* If the user chose to use the Standard Tool Smash Tile (First Page) */
+    STANDARD_TOOL_SMASH_TILE,
+    /* If the user chose to use the Special Tool Swap Tiles (First Page) */
+    STANDARD_TOOL_SWAP_TILES,
+    /* If the user chose to use the Special Tool Change Value (First Page) */
+    SPECIAL_TOOL_CHANGE_VALUE,
+    /* If the user chose to use the Special Tool Eliminate Value (First Page) */
+    SPECIAL_TOOL_ELIMINATE_VALUE,
+    /* If the user chose to use the Special Tool Destroy Area (First Page) */
+    SPECIAL_TOOL_DESTROY_AREA,
+
+    /* If the user wants to return to the main menu (Second Page) */
     MAIN_MENU,
-
-    /* If the user wants to play the game again */
+    /* If the user wants to play the game again (Second Page) */
     PLAY_AGAIN,
-
-    /* User has made one or more moves and he/she wants to undo the last move */
-    UNDO_LAST_MOVE
 }
